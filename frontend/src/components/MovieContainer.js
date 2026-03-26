6b0431a178650955d4f7969b4327513ad9b1e480
Nishant Sharma <nishantsharma5330@gmail.com>
2026-03-27 00:13:13 +0530

    add

--- frontend/src/components/MovieContainer.js
+++ frontend/src/components/MovieContainer.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import MovieList from './MovieList'
+import {useSelector} from "react-redux"
+
 
 function MovieContainer() {
+  const movie= useSelector((store)=>store.movie)
+  
+  
   return (
-    <div>
+    <div className='bg-black'>
+      <div className='relative z-10 -mt-52'>
+     <MovieList title={"Popular Movies"} movies={movie.popularMovie}/>
+     <MovieList title={"Now Playing Movies"} movies={movie.nowPlayingMovie}/>
+     <MovieList title={"Top Rated Movie"} movies={movie. topRatedMovie}/>
+     <MovieList title={"Upcoming Movie"} movies={movie. upcomingMovie}/>
+      </div>
+      console.log(movies);
       
+     
     </div>
   )
 }
