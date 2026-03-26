ceb8df16888331250b2315aec80ecf1eda33e897
Nishant Sharma <nishantsharma5330@gmail.com>
2026-03-26 17:19:00 +0530

    Add MainContainer and MovieContainer to Browse component

--- frontend/src/components/Browse.js
+++ frontend/src/components/Browse.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import Header from './Header'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
+import MainContainer from './MainContainer';
+import MovieContainer from './MovieContainer';
 
 function Browse() {
   const user = useSelector(store=>store.app.user);
@@ -17,7 +19,8 @@ function Browse() {
     <div>
      <Header/>
      <div>
-      browse
+     <MainContainer/>
+     <MovieContainer/>
      </div>
     </div>
   )
