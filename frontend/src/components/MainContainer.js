dd229eb9236529a630237fb4721d7949d4af4d89
Nishant Sharma <nishantsharma5330@gmail.com>
2026-03-28 08:18:29 +0530

    finished the project

--- frontend/src/components/MainContainer.js
+++ frontend/src/components/MainContainer.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import ViedoTitle from './ViedoTitle'
 import ViedoBackground from './ViedoBackground'
+import { useSelector } from 'react-redux'
 
 function MainContainer() {
+  const movie = useSelector(store=>store.movie?.nowPlayingMovie)
+  if(!movie) return;
+
+  console.log(movie);
+  
+  const {overview, title, id} = movie[0]
+
   return (
-    <div>
-        <ViedoTitle/>
-        <ViedoBackground/>
+    <div className=' overflow-x-hidden'>
+        <ViedoTitle title={title} overview={overview}/>
+        <ViedoBackground movieId={id}/>
       
     </div>
   )
