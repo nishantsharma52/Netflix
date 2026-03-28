72540c872dda01e18f60e5f1c9b06ddd3f6c7630
Nishant Sharma <nishantsharma5330@gmail.com>
2026-03-28 08:18:56 +0530

    finished the project

--- frontend/src/components/SearchMovie.js
+++ frontend/src/components/SearchMovie.js
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from "axios";
+import { SEARCH_MOVIE_URL, options } from '../utils/constant';
+import { useDispatch, useSelector } from "react-redux";
+import { setSearchMovieDetails } from '../redux/searchSlice';
+import { setLoading } from '../redux/userSlice';
+import MovieList from './MovieList';
 
 const SearchMovie = () => {
+  const [searchMovie, setSearchMovie] = useState("");
+  const dispatch = useDispatch();
+  const isLoading = useSelector(store => store.app.isLoading);
+  const { movieName, searchedMovie } = useSelector(store => store.searchMovie);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    dispatch(setLoading(true));
+    try {
+      const res = await axios.get(`${SEARCH_MOVIE_URL}${searchMovie}&include_adult=false&language=en-US&page=1`, options);
+      const movies = res?.data?.results;
+      dispatch(setSearchMovieDetails({ searchMovie, movies }));
+    } catch (error) {
+      console.log(error);
+    } finally {
+      dispatch(setLoading(false));
+    }
+    setSearchMovie("");
+  }
+
   return (
-    <div>SearchMovie</div>
+    <>
+      <div className='flex justify-center pt-[10%] w-[100%]'>
+        <form onSubmit={submitHandler} className='w-[50%]'>
+          <div className='flex justify-between shadow-md border-2 p-2 border-gray-200 rounded-lg w-[100%]'>
+            <input value={searchMovie} onChange={(e) => { setSearchMovie(e.target.value) }} className='w-full outline-none rounded-md text-lg' type="text" placeholder='Search Movies...' />
+            <button className='bg-red-800 text-white rounded-md px-4 py-2'>{isLoading ? "loading..." : "Search"}</button>
+          </div>
+        </form>
+
+      </div>
+      {/* 1. Jab movie mil jaye (Data null na ho aur length 0 se zyada ho) */}
+      {searchedMovie !== null && searchedMovie.length > 0 && (
+        <MovieList title={movieName} searchMovie={true} movies={searchedMovie} />
+      )}
+
+      {/* 2. Jab movie NA mile (Data null na ho par length 0 ho jaye) */}
+      {searchedMovie !== null && searchedMovie.length === 0 && (
+        <div className='flex justify-center items-center h-[50vh]'>
+          <h1 className='text-3xl font-bold text-black'>Movie Not Found!!</h1>
+        </div>
+      )}
+
+    </>
+
   )
 }
 
