0e221eb4a233e732f07cfe74af75e5dc7a9bc5b8
Nishant Sharma <nishantsharma5330@gmail.com>
2026-03-28 08:18:49 +0530

    finished the project

--- frontend/src/components/MovieList.js
+++ frontend/src/components/MovieList.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import MovieCard from './MovieCard'
 
-const MovieList = ({title, movies}) => {
+const MovieList = ({title, movies, searchMovie=false}) => {
   return (
     <div className='px-8'>
-        <h1 className=' text-3xl py-3 text-white'>{title}</h1>
+        <h1 className={`text-3xl ${searchMovie ? "text-black" :" text-white"} py-3`}>{title}</h1>
         <div className=' flex cursor-pointer overflow-x-auto  no-scrollbar'>
             <div className=' flex items-center'>
                 {
                     movies?.map((movie) =>{
                         return(
-                            <MovieCard key={movie.id} posterPath={movie.poster_path}/>
+                            <MovieCard movieId={movie.id} posterPath={movie.poster_path}/>
                         )
                     })
                 }
