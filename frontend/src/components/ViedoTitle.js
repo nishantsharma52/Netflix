cb1e7390e0224cee12024172bbbb1c30110d2066
Nishant Sharma <nishantsharma5330@gmail.com>
2026-03-28 08:19:08 +0530

    finished the project

--- frontend/src/components/ViedoTitle.js
+++ frontend/src/components/ViedoTitle.js
@@ -3,11 +3,11 @@ import { CiPlay1 } from "react-icons/ci";
 import { CiCircleInfo } from "react-icons/ci";
 
 
-const ViedoTitle = () => {
+const ViedoTitle = ({title, overview}) => {
     return (
-        <div className=' w-screen aspect-video absolute text-white pt-[18%] p-12'>
-            <h1 className='text-3xl font-bold'>Nishant Sharma</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, qui.</p>
+        <div className='w-screen absolute text-white pt-[18%] p-12'>
+            <h1 className='text-3xl font-bold'>{title}</h1>
+            <p className='w-1/3 mt-4'>{overview}</p>
             <div className=' flex  mt-8'>
                 <button className=' flex items-center px-6 py-2 bg-white text-black rounded-md hover:bg-opacity-80'>
                     <CiPlay1 size="24px" />
