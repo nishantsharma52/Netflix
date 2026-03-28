083dd0935591c7a6e69483db58469139a9a551df
Nishant Sharma <nishantsharma5330@gmail.com>
2026-03-28 08:18:36 +0530

    finished the project

--- frontend/src/components/MovieCard.js
+++ frontend/src/components/MovieCard.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import { Banner_Url } from '../utils/constant'
+import { useDispatch } from 'react-redux'
+import { getId, setOpen } from '../redux/movieSlice'
 
-const MovieCard = ({posterPath}) => {
+const MovieCard = ({ posterPath, movieId }) => {
+    const dispatch = useDispatch()
+
+    if (posterPath === null) return null
+    const handleOpen = () => {
+        dispatch(getId(movieId))
+        dispatch(setOpen(true))
+
+    }
     return (
-        <div className='w-48 pr-2'>
+        <div onClick={handleOpen} className='w-48 pr-2 '>
             <img src={`${Banner_Url}/${posterPath}`} alt="movie" />
         </div>
     )
