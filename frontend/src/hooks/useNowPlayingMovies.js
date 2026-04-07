74a3a741ad5533f6440c16c11d349cb6cb4b8f0f
Nishant Sharma <nishantsharma5330@gmail.com>
2026-04-07 19:17:30 +0530

    add update

--- frontend/src/hooks/useNowPlayingMovies.js
+++ frontend/src/hooks/useNowPlayingMovies.js
@@ -1,19 +1,26 @@
- import axios from "axios";
- import { getNowPlayingMovie } from "../redux/movieSlice";
- import { Now_Playing_Movie, options } from "../utils/constant";
- import {useDispatch} from "react-redux"
- 
- const useNowPlayingMovies = async () => {
-    const dispatch = useDispatch()
-    try {
-      const res = await axios.get(Now_Playing_Movie, options) 
-     
-      dispatch(getNowPlayingMovie(res.data.results))
-      
-    } catch (error) {
-      console.log(error);
-      
-      
+import axios from "axios";
+import { getNowPlayingMovie } from "../redux/movieSlice";
+import { Now_Playing_Movie, options } from "../utils/constant";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react"; // 1. useEffect import karna zaroori hai
+
+const useNowPlayingMovies = () => { // 2. Yahan se 'async' hata do
+    const dispatch = useDispatch();
+
+    const getNowPlayingMovies = async () => {
+        try {
+            const res = await axios.get(Now_Playing_Movie, options);
+            // Redux store mein movies save karo
+            dispatch(getNowPlayingMovie(res.data.results));
+        } catch (error) {
+            console.log(error);
+        }
     }
-  }
-   export default useNowPlayingMovies
+
+    useEffect(() => {
+        // 3. API call ko useEffect ke andar rakho
+        getNowPlayingMovies();
+    }, []); // 4. Ye [] array sabse zaroori hai, taaki loop na bane
+}
+
+export default useNowPlayingMovies;
