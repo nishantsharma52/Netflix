9623d3307ab94dc2e6113290a235075cef6b5f8b
Nishant Sharma <nishantsharma5330@gmail.com>
2026-04-17 18:57:44 +0530

    updated code

--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Login from './Login'
 import Browse from "./Browse"
